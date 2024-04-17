344eb6ebd0f3ba7923d49233de5664fe23008b76
DamlaGurel <damla.gureel@gmail.com>
2024-04-17 15:44:34 +0300

    entity klasörü oluşturuldu

---
+++ IKProjesiAPI.Domain/Entities/Company.cs
@@ -0,0 +1,30 @@
+﻿using IKProjesiAPI.Domain.Enums;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IKProjesiAPI.Domain.Entities
+{
+    public class Company
+    {
+        public string CompanyName { get; set; }
+        public string CompanyTitle { get; set; }
+        public string MersisNumber { get; set; }
+        public string CompanyTaxNumber { get; set; }
+        public string CompanyTaxOffice { get; set; }
+        public IFormFile Logo { get; set; }
+        public string LogoPath { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+        public int EmployeeNumber { get; set; }
+        public DateTime FoundationYear { get; set; }
+        public DateTime StartContractDate { get; set; }
+        public DateTime EndContractDate { get; set; }
+        public Status Status { get; set; }
+
+    }
+}
---
+++ IKProjesiAPI.Domain/Entities/IBaseEntity.cs
@@ -0,0 +1,18 @@
+﻿using IKProjesiAPI.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IKProjesiAPI.Domain.Entities
+{
+    public interface IBaseEntity
+    {
+        public DateTime CreatedDate { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+        public DateTime? DeletedDate { get; set; }
+
+        public Status Status { get; set; }
+    }
+}
