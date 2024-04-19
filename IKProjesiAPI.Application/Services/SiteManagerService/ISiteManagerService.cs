d71899f0dd726bd642eacb67f5afb5da4e14035a
siilayildirim <sila07yildirim@gmail.com>
2024-04-19 13:30:53 +0300

    ISiteManagerService eklendi

--- IKProjesiAPI.Application/Services/SiteManagerService/ISiteManagerService.cs
+++ IKProjesiAPI.Application/Services/SiteManagerService/ISiteManagerService.cs
@@ -1,7 +1,17 @@
 ﻿
+using IKProjesiAPI.Application.Models.DTOs.SiteManagerDTOs;
+using IKProjesiAPI.Domain.Entities;
+
 namespace IKProjesiAPI.Application.Services.SiteManagerService
 {
     public interface ISiteManagerService
     {
+        Task Add(AddSiteManagerDto model);
+        Task<SiteManager> GetSiteManager(int id);
+        Task<SiteManagerDetailsDto> GetSiteManagerDetails(int id);
+        Task<SiteManagerSummaryDto> GetSiteManagerSummary(int id);
+        Task Update(SiteManagerUpdateDto model);
+        Task Delete(int id);
+
     }
 }
