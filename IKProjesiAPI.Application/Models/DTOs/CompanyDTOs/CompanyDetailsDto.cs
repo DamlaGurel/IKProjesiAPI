59788ed921f057423c74c0b720e152995c4f3fb0
ecek24 <eceekarakuss@gmail.com>
2024-04-26 02:40:57 +0300

    logo finito

--- IKProjesiAPI.Application/Models/DTOs/CompanyDTOs/CompanyDetailsDto.cs
+++ IKProjesiAPI.Application/Models/DTOs/CompanyDTOs/CompanyDetailsDto.cs
@@ -16,8 +16,8 @@ namespace IKProjesiAPI.Application.Models.DTOs.CompanyDTOs
         public string MersisNumber { get; set; }
         public string CompanyTaxNumber { get; set; }
         public string CompanyTaxOffice { get; set; }
-        //public IFormFile Logo { get; set; }
-        //public string LogoPath { get; set; }
+        public string LogoString { get; set; }
+        public byte[]? LogoBytes { get; set; }
         public string PhoneNumber { get; set; }
         public string Address { get; set; }
         public string Email { get; set; }
