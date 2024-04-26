19bead79d910ac03feace1bba50a53d96dd79237
ilkay <arslanilkay06@gmail.com>
2024-04-26 12:27:20 +0300

    geçiçi şifre işlemleri

--- IKProjesiAPI.Domain/Entities/TemporaryPassword.cs
+++ IKProjesiAPI.Domain/Entities/TemporaryPassword.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using IKProjesiAPI.Domain.Entities.AppEntities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,5 +11,6 @@ namespace IKProjesiAPI.Domain.Entities
     {
         public int Id { get; set; }
         public string Password { get; set; }
+        public AppUser AppUser { get; set; }
     }
 }
