b437c1c4515f86409df968bb06bfd68cfd6a0421
siilayildirim <sila07yildirim@gmail.com>
2024-05-02 00:43:03 +0300

    moneyType enum tanımlandı

--- IKProjesiAPI.Domain/Enums/MoneyType.cs
+++ IKProjesiAPI.Domain/Enums/MoneyType.cs
@@ -1,8 +1,31 @@
-﻿namespace IKProjesiAPI.Domain.Enums
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IKProjesiAPI.Domain.Enums
 {
 	public enum MoneyType
 	{
-
-	}
+        [Display(Name = "Türk Lirası")]
+        TRY = 1,
+        [Display(Name = "ABD Doları")]
+        USD,
+        [Display(Name = "Avro")]
+        EUR,
+        [Display(Name = "İngiliz Sterlini")]
+        GBP,
+        [Display(Name = "Japon Yeni")]
+        JPY,
+        [Display(Name = "İsviçre Frangı")]
+        CHF,
+        [Display(Name = "Avustralya Doları")]
+        AUD,
+        [Display(Name = "Kanada Doları")]
+        CAD,
+        [Display(Name = "Yeni Zelanda Doları")]
+        NZD,
+        [Display(Name = "Çin Yuanı")]
+        CNY,
+        [Display(Name = "Brezilya Reali")]
+        BRL,
+    }
 }
 
