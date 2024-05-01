742d3c111098a584f4ca93f99a0ea2a86dadc048
ecek24 <eceekarakuss@gmail.com>
2024-05-01 14:35:12 +0300

    company manager icin duzenleme

--- IKProjesiAPI.Application/Models/DTOs/CompanyManagerDTOs/ListCompanyManagerDto.cs
+++ IKProjesiAPI.Application/Models/DTOs/CompanyManagerDTOs/ListCompanyManagerDto.cs
@@ -15,12 +15,9 @@ namespace IKProjesiAPI.Application.Models.DTOs.CompanyManagerDTOs
         public string LastName { get; set; }
         public string SecondLastName { get; set; }
         public string Email { get; set; }
-        public string Address { get; set; }
         public string PhoneNumber { get; set; }
         public string CompanyName { get; set; }
         public int? CompanyId { get; set; }
-        public string? ImageString { get; set; }
-        public byte[]? ImageBytes { get; set; }
     }
 }
 
---
+++ IKProjesiAPI.Application/Models/DTOs/PersonelDTOs/PersonelSummaryDto.cs
@@ -0,0 +1,27 @@
+﻿using IKProjesiAPI.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IKProjesiAPI.Application.Models.DTOs.PersonelDTOs
+{
+    public class PersonelSummaryDto
+    {
+        //Foto, Ad, Soyad, Email, Telefon, Adres,Meslek, Departman
+
+        public string? LogoString { get; set; }
+        public byte[]? LogoBytes { get; set; }
+        public string? FirstName { get; set; }
+        public string? SecondName { get; set; }
+        public string? LastName { get; set; }
+        public string? SecondLastName { get; set; }
+        public string? Email { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? Address { get; set; }
+        public Job? JobName { get; set; }
+        public Department? DepartmentName { get; set; }
+
+    }
+}
