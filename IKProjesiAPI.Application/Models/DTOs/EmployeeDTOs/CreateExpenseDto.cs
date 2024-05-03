bf755529661d49ecc3fd961d88102d46571c6e25
siilayildirim <sila07yildirim@gmail.com>
2024-05-03 18:51:34 +0300

    Expense enumları için metot yazıldı. CreateExpense metotları güncellendi

--- IKProjesiAPI.Application/Models/DTOs/EmployeeDTOs/CreateExpenseDto.cs
+++ IKProjesiAPI.Application/Models/DTOs/EmployeeDTOs/CreateExpenseDto.cs
@@ -1,20 +1,17 @@
 ﻿
 using IKProjesiAPI.Domain.Entities;
 using IKProjesiAPI.Domain.Enums;
+using Microsoft.AspNetCore.Http;
 
 namespace IKProjesiAPI.Application.Models.DTOs.EmployeeDTOs
 {
     public class CreateExpenseDto
     {
-        public int? EmployeeId { get; set; }
-        public Employee? Employee { get; set; }
-
-        public ExpenseType? ExpenseType { get; set; }
+        public int ExpenseTypeId { get; set; }
+        //public ExpenseType? ExpenseType { get; set; }
         public double? TotalExpense { get; set; }
-        public MoneyType? MoneyType { get; set; }
-        public ApprovalType? ApprovalType { get; set; }
-        public DateTime? ResponseDate { get; set; }
-        public DateTime? RequestDate { get; set; }
-        //public string dosyaeklemepropu { get; set; }
+        //public MoneyType? MoneyType { get; set; }
+        public int MoneyTypeId { get; set; }
+        //public IFormFile? File { get; set; }
     }
 }
