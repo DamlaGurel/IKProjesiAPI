83fac7715c8748499c7b649ac236cfa8b4224a01
ilkay <arslanilkay06@gmail.com>
2024-05-12 15:32:12 +0300

    update

--- IKProjesiAPI.Application/Services/AppUserService/IAppUserService.cs
+++ IKProjesiAPI.Application/Services/AppUserService/IAppUserService.cs
@@ -6,9 +6,7 @@ namespace IKProjesiAPI.Application.Services.AppUserService
     public interface IAppUserService
     {
         Task<SignInResult> Login(LoginDto model);
-        //Task<bool> ForgotPassword(LoginDto model);
         Task ChangePassword(ChangePasswordDto password);
-
         Task LogOut();
     }
 }
